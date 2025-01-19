2c66d7ebedf8ff76e05a1a2de708137fe5db89af
teja-rks <rksteja234@gmail.com>
2025-01-19 13:34:00 +0530

    third  commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import SortingVisualizer from './components/SortingVisualizer';
 function App() {
   return (
     <div className="App">
-      <h1>Sorting Visualizer</h1>
       <SortingVisualizer />
     </div>
   );
